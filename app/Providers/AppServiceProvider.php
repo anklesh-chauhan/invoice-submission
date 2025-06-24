562da21bf3cf911755e27eb256c309305ea81e97
anklesh-chauhan <your-email@example.com>
2025-06-24 10:52:56 +0530

    123

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Gate;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -17,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
     {
-        //
+        Gate::define('viewFilament', function ($user) {
+            return true; // ✅ Allow all authenticated users
+        });
     }
 }
